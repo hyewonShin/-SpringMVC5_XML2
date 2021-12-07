be1c8c25248cd80b17c1521f9f43c5903c6884da
hyewonShin <rusy7225@naver.com>
2021-12-07 20:14:39 +0900

    회원가입 - 아이디 중복 확인 처리 아이디 중복 확인 처리를 구현합니다. RestController를 사용합니다. 클라이언트는 Ajax 통신을 사용하여 처리합니다.

---
+++ MiniProjectXML/src/kr/co/hyewon/dao/UserDao.java
@@ -0,0 +1,17 @@
+package kr.co.hyewon.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDao {
+
+	// xml에서 mapper 주입받는 법 
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	public String checkUserIdExist(String user_id) {
+		return sqlSessionTemplate.selectOne("user.checkUserIdExist", user_id);
+	}
+}
